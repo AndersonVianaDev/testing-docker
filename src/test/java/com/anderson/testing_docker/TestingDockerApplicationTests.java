d60e9244f5403959aaf7bee2cef4e5f5b50f23e8
AndersonVianaDev <andersonp.viana.contato@gmail.com>
2024-07-30 17:19:07 -0300

    feat: creating project

---
+++ src/test/java/com/anderson/testing_docker/TestingDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.testing_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
